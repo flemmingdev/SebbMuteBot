846f754bdab69ff1bc3b6deb5d82f2cd9280c3ab
Flemming Dammann <f.dammann@stud.hs-wismar.de>
2024-04-11 21:07:27 +0200

    fix

--- sebbmutebot.py
+++ sebbmutebot.py
@@ -1,11 +1,15 @@
 import discord
+from datetime import datetime
 
-TOKEN = 'MTIyODA0MTcxMDE5MDQ2NTA5NQ.G1efdl.eXHrrmzTSUCqgDdvFDZRKnodtdalug-0XBJ1ik' #discord bot token
+intents = discord.Intents.default()
+intents.voice_states = True
+
+TOKEN = 'MTIyODA0MTcxMDE5MDQ2NTA5NQ.G_XFCX.O31YyIn063LGsnQ5dkQ8R4YeGUMHRBCS2S13Rs' #discord bot token
 SERVER_ID = '372827135594725376' #discord server id
 CHANNEL_ID = '1227333701554143252' #discord server kanal id
 TARGET_USER_ID = '424232360960196621' #discord nutzer id
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -13,16 +17,19 @@ async def on_ready():
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    if before.self_mute != after.self_mute or before.self_deaf != after.self_deaf:
-        if after.self_mute:
-            message = f'{member.name} ist nun muted.'
-        elif after.self_deaf:
-            message = f'{member.name} ist nun full mute.'
-        else:
-            message = f'{member.name} ist nun wieder entmuted.'
+    if member.id == int(TARGET_USER_ID):
+        if before.self_mute != after.self_mute or before.self_deaf != after.self_deaf:
+            now = datetime.now()
+            timestamp = now.strftime("%H:%M Uhr")
+            if after.self_mute:
+             message = f'{member.name} hat sich um {timestamp} gemuted.'
+            elif after.self_deaf:
+             message = f'{member.name} hat sich um {timestamp} full muted.'
+            else:
+             message = f'{member.name} hat sich um {timestamp} entmuted.'
         
-        guild = client.get_guild(int(SERVER_ID))
-        channel = guild.get_channel(int(CHANNEL_ID))
+            guild = client.get_guild(int(SERVER_ID))
+            channel = guild.get_channel(int(CHANNEL_ID))
 
         await channel.send(message)
 
