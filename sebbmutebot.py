6434b6cd0ee98e2f4181d106cc09888c7d51cd90
Flemming Dammann <f.dammann@stud.hs-wismar.de>
2024-04-11 22:59:57 +0200

    data anonimized

--- sebbmutebot.py
+++ sebbmutebot.py
@@ -4,10 +4,10 @@ from datetime import datetime
 intents = discord.Intents.default()
 intents.voice_states = True
 
-TOKEN = 'MTIyODA0MTcxMDE5MDQ2NTA5NQ.G_XFCX.O31YyIn063LGsnQ5dkQ8R4YeGUMHRBCS2S13Rs' #discord bot token
-SERVER_ID = '372827135594725376' #discord server id
-CHANNEL_ID = '1227333701554143252' #discord server kanal id
-TARGET_USER_ID = '424232360960196621' #discord nutzer id
+TOKEN = 'INSERT BOT TOKEN' #discord bot token
+SERVER_ID = '' #insert discord server id
+CHANNEL_ID = '' #insert discord server channel id
+TARGET_USER_ID = '' #insert discord user id
 
 client = discord.Client(intents=intents)
 
